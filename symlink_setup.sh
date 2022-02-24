f1ebf2c16e6425126ca1a60839c2a62e3ef654a9
Patrick Michel <maiL@patrickmichel.de>
2022-02-24 09:59:53 +0100

    fixed symlink script

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -6,9 +6,10 @@ mkdir -p "$HOME/.config/nvim/lua"
 
 for file in $(find . -type f -not -path '*/\.git/*' -not -name 'README.md' -not -name 'symlink_setup.sh'); do
   stripped=${file:2}
+  source=$(echo "$PWD/$stripped")
   target=$(echo "$HOME/$stripped")
-  echo "$file -> $target"
-  ln -sf $file $target
+  echo "$source -> $target"
+  ln -sf $source $target
 done
   
 
