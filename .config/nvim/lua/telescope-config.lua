e72cfe968e0f7c922d061d336e99fd5025975926
Patrick Michel <michel@transporeon.com>
2022-03-22 10:07:15 +0100

    updated telescope picker style a bit

--- .config/nvim/lua/telescope-config.lua
+++ .config/nvim/lua/telescope-config.lua
@@ -1,6 +1,32 @@
 require('telescope').setup {
+  -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
   defaults = {
-    path_display = {'tail'}
+    path_display = {'tail'},
+    path_display = { "truncate" },
+    winblend = 0,
+    border = {},
+    borderchars = { "─", "│", "─", "│", '┌', '┐', '┘', '└' },
+    color_devicons = true,
+    file_ignore_patterns = { "node_modules" },
+    layout_strategy = "horizontal",
+    layout_config = {
+       horizontal = {
+          prompt_position = "bottom",
+          preview_width = 0.55,
+          results_width = 0.8,
+       },
+       vertical = {
+          mirror = false,
+       },
+       width = 0.4,
+       height = 0.8,
+       preview_cutoff = 120,
+    },
+  },
+  pickers = {
+    find_files = {
+
+    }
   },
   extensions = {
     fzf = {
@@ -19,7 +45,9 @@ require('telescope').load_extension('fzf')
 
 -- TEMPORARY function to use find_files as fallback, should be in separate file (telescope_config.lua)
 project_files = function()
-  local opts = {} -- define here if you want to define something
+  local opts = {
+    previewer = false
+  } -- define here if you want to define something
   local ok = pcall(require'telescope.builtin'.git_files, opts)
   if not ok then require'telescope.builtin'.find_files(opts) end
 end
