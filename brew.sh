29823f9260d7c335933188a2199fbba56f06ff68
Patrick M <mail@patrickmichel.de>
2022-05-01 14:47:41 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@ brew install fd
 brew install fzf
 brew install lazygit
 brew install lua
-brew instal lua-language-server
+brew install lua-language-server
 brew install luajit
 brew install luajit-openresty
 brew install ruby
@@ -15,8 +15,5 @@ brew install bat
 
 # casks
 
-brew install --cask brave-browser
 brew install --cask iterm2
-brew install --cask vlc
 brew install --cask font-hack-nerd-font
-brew install --cask amethyst
