95c9d6a921146036322e3dc136f999fa394b6760
Patrick Michel <maiL@patrickmichel.de>
2022-12-21 07:03:30 +0100

    updated mason config

--- .config/nvim/lua/mason-config.lua
+++ .config/nvim/lua/mason-config.lua
@@ -1,3 +1,11 @@
 require('mason').setup()
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'pyright', 'tsserver', 'dockerls', 'cssls',
+        'tailwindcss', 'ansiblels', 'gopls', 'gradle_ls',
+        'jsonls', 'rust_analyzer', 'sumneko_lua',
+        'angularls', 'jdtls', 'yamlls', 'marksman', 'pyright', 'gradle_ls'
+    },
+    automatic_installation = true
+})
 require('nvim-lsp-config')
