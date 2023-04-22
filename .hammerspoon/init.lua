d565a9d16751cfcacd3ec0dd7190a3821fbfadd0
Patrick Michel <patrick@Patricks-MBP.fritz.box>
2023-04-22 09:05:11 +0200

    feat: added hotkeys via hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,12 @@
+-- Shortcuts to open / focus applications
+local bindToCtrlLaunchOrFocus = function(key, application)
+    hs.hotkey.bind({"alt"}, key, function()
+    hs.application.launchOrFocus(application)
+  end)
+end
+bindToCtrlLaunchOrFocus("1", "kitty")
+bindToCtrlLaunchOrFocus("2", "IntelliJ IDEA")
+-- bindToCtrlLaunchOrFocus("3", "")
+bindToCtrlLaunchOrFocus("4", "Brave Browser")
+bindToCtrlLaunchOrFocus("5", "Slack")
+bindToCtrlLaunchOrFocus("6", "Spotify")
