6e06894f144f66d10c4e1cff31efcad5f71d37b1
Patrick Michel <patrick.michel@minubo.com>
2023-07-19 07:12:12 +0200

    set Obsidian to opt+3

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,7 +6,7 @@ local bindToCtrlLaunchOrFocus = function(key, application)
 end
 bindToCtrlLaunchOrFocus("1", "kitty")
 bindToCtrlLaunchOrFocus("2", "IntelliJ IDEA")
--- bindToCtrlLaunchOrFocus("3", "")
+bindToCtrlLaunchOrFocus("3", "Obsidian")
 bindToCtrlLaunchOrFocus("4", "Brave Browser")
 bindToCtrlLaunchOrFocus("5", "Slack")
 bindToCtrlLaunchOrFocus("6", "Spotify")
