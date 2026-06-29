16ebad4d54e7baf2c6324e513edbb3f0b9bdcdad
Patrick M <patrick.michel@minubo.com>
2026-06-29 14:27:23 +0200

    changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,19 +1,69 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = 'carbonfox'
-
 config.font_size = 14.0
-
 config.font = wezterm.font_with_fallback {
-    {family='Cascadia Code', weight=600}
+    { family = 'Cascadia Code', weight = 600 }
 }
+
+-- Hide macOS title bar, use integrated buttons
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.integrated_title_button_alignment = "Left"
+config.macos_window_background_blur = 0
+
+-- Minimal flat tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 32
+
+-- Carbonfox-matched tab bar colors
+config.colors = {
+    tab_bar = {
+        background = '#161616',
+        active_tab = {
+            bg_color = '#282828',
+            fg_color = '#f2f4f8',
+            intensity = 'Normal',
+            underline = 'None',
+            italic = false,
+            strikethrough = false,
+        },
+        inactive_tab = {
+            bg_color = '#161616',
+            fg_color = '#6e6f70',
+        },
+        inactive_tab_hover = {
+            bg_color = '#1c1c1c',
+            fg_color = '#a8aab0',
+        },
+        new_tab = {
+            bg_color = '#161616',
+            fg_color = '#6e6f70',
+        },
+        new_tab_hover = {
+            bg_color = '#1c1c1c',
+            fg_color = '#f2f4f8',
+        },
+    },
+}
+
+-- Clean tab title: just the process name or title, no index prefix
+wezterm.on('format-tab-title', function(tab, _, _, _, _, _)
+    local title = tab.active_pane.title
+    -- Trim to keep it short
+    if #title > 28 then
+        title = title:sub(1, 26) .. '…'
+    end
+    local pad = ' '
+    if tab.is_active then
+        return pad .. title .. pad
+    else
+        return pad .. title .. pad
+    end
+end)
 
 config.keys = {
     {
@@ -28,5 +78,4 @@ config.keys = {
     },
 }
 
--- and finally, return the configuration to wezterm
 return config
